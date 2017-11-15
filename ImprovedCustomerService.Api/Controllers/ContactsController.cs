a088a5ae92aebbdbf6832fa0dcafa8cb1b472b38
Brett Parvin <bparvin@arrivelogistics.com>
2017-11-15 13:31:01 -0600

    added skeleton contacts controller and skeleton GetAll endpoint

---
+++ ImprovedCustomerService.Api/Controllers/ContactsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ImprovedCustomerService.Api.Controllers
+{
+    [RoutePrefix("api/Contacts")]
+    public class ContactsController : ApiController
+    {
+        [Route("GetAll")]
+        public HttpResponseMessage GetAll()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, "success");
+        }
+    }
+}
